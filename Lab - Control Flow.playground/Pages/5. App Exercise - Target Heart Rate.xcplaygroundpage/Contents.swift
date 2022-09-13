8aefde33b306cf1fc10fd00277f84afb9e901efc
Mason Guaman <masonstalin@gmail.com>
2022-09-13 13:58:06 -0400

    th

--- Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
+++ Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
@@ -10,7 +10,18 @@
 let targetLowerBound = 120
 let targetUpperBound = 150
 let currentHR = 147
- 
+let isInTarget = false
+let isBelowTarget = false
+let isAboveTarget = false
+if currentHR < targetUpperBound && currentHR > targetLowerBound {
+    print("You're right on track!")
+}
+else if currentHR < targetLowerBound {
+    print("You're doing great, but try to push it a bit!")
+}
+else if currentHR > targetUpperBound {
+    print("You're on fire! Slow it down just a bit.")
+}
 /*:
 [Previous](@previous)  |  page 5 of 9  |  [Next: Exercise - Switch Statements](@next)
  */
--- Lab - Control Flow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
+++ Lab - Control Flow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
@@ -16,6 +16,20 @@
  If `currentHR` is above the listed zones, print some kind of warning asking the user to slow down.
  */
 let currentHR = 128
+switch currentHR {
+case 100...129:
+    print("You are in the Very Light zone. Activity in this zone helps with recovery.")
+case 120...140:
+    print("You are in the Light zone. Activity in this zone helps improve basice endurance and fat burning.")
+case 140...160:
+    print("You are in the Moderate zone. Activity in this zone helps improve aerobic fitness.")
+case 160...180:
+    print("You are in the Hard zone. Activity in this zone increases maximum performance capacity for shorter sessions.")
+case 180...200:
+    print("You are in the Maximum zone. Activity in this zone helps fit athletes develop speed.")
+default:
+    print("You should slow down")
+}
 
 /*:
 [Previous](@previous)  |  page 7 of 9  |  [Next: Exercise - Ternary Operator](@next)
